83e144542507a83786a32dc4ecdbcd917a16510c
anhhuy2110 <bahuy21102001@gmail.com>
2025-09-25 11:16:43 +0700

    Base backend

---
+++ backend/api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using api.Entities;
+using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hasPssword = new PasswordHasher<User>()
+                .HashPassword(user, user.Password);
+            user.Username = request.Username;
+            user.Password = hasPssword;
+            return Ok(user);
+        }
+    }
+}
---
+++ backend/api/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;    
+    }
+}
