15590066522e3e2a5a4a98b0502b16cf07144660
anhhuy2110 <bahuy21102001@gmail.com>
2025-09-25 11:59:28 +0700

    Implement the login Endpint

--- backend/api/Controllers/AuthController.cs
+++ backend/api/Controllers/AuthController.cs
@@ -10,16 +10,36 @@ namespace api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public User user = new();
+        public static User user = new();
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserDto request)
         {
             var hasPssword = new PasswordHasher<User>()
-                .HashPassword(user, user.Password);
+                .HashPassword(user, request.Password);
             user.Username = request.Username;
-            user.Password = hasPssword;
+            user.PasswordHash = hasPssword;
             return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            var verifyPassword = new PasswordHasher<User>()
+                .VerifyHashedPassword(user, user.PasswordHash, request.Password);
+
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (verifyPassword == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = "success";
+
+            return Ok(token);
         }
     }
 }
